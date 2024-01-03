0e2959e937ce330a226d1f9a2def20c6a2749b3b
Farhan933 <farhanfarabiemon@gmail.com>
2024-01-03 22:28:52 +0600

    super power of initialization and print of a string

---
+++ Week 02/Module 10/initialize_of_string.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char a[6] = "Rahat"; //super power
+    printf("%s", a); //super power eita shudu matro string er belay.
+    return 0;
+}
+
+// {size 1 baray dite hoy, null neyar jonno}
